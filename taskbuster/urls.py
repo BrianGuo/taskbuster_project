2348bc95e5e472d4360cc5e850d23f4a088dca47
samuelcg <samuel.gonzalesluna@gmail.com>
2016-04-13 20:09:13 -0500

    Refactor template inheritance, render webstie files and begin testing with coverage.

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from.views import home
+from.views import home, home_files
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'taskbuster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(
+        r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files,
+        name='home-files'
+    ),
     url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
