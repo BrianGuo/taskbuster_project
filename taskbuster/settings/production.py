c36b96e1b547fb80942122af781ea2627d53ab9b
samuelcg <samuel.gonzalesluna@gmail.com>
2016-04-16 17:06:08 -0500

    Configure production setting for heroku.

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS_ = ['taskbustertutsgl.herokuapp.com']
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
