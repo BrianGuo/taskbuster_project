2f8d1717ebcd6dce89f13b1c976d078c268f29fb
samuelcg <samuel.gonzalesluna@gmail.com>
2016-04-16 17:14:31 -0500

    SITE_ID for prod.

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -5,6 +5,7 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+SITE_ID = 2
 ALLOWED_HOSTS_ = ['taskbustertutsgl.herokuapp.com']
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
