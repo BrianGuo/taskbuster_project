6216886e5f0e4f45dd8ded48aa0e28657d5a4a12
samuelcg <samuel.gonzalesluna@gmail.com>
2016-04-16 23:55:40 -0500

    Heroku databse con_max_age setting.

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -7,7 +7,7 @@ DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 SITE_ID = 3
 ALLOWED_HOSTS_ = ['taskbustertutsgl.herokuapp.com']
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
